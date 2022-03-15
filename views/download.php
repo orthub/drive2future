92b1f122c9dab9bda2d542e1d04cb1a849cddca1
Roland.Ortner <ortner.roland@outlook.at>
2022-03-15 09:12:03 +0100

    added navbar

--- views/download.php
+++ views/download.php
@@ -3,7 +3,7 @@
 <?php require_once __DIR__ . '/partials/head.php' ?>
 
 <body>
-
+  <?php require_once __DIR__ . '/partials/navbar.php' ?>
 
 
 
---
+++ views/upload.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require_once __DIR__ . '/partials/head.php' ?>
+
+<body>
+  <?php require_once __DIR__ . '/partials/navbar.php' ?>
+
+
+
+
+
+  <?php require_once __DIR__ . '/partials/footer.php' ?>
+</body>
+
+</html>
