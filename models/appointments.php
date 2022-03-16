2a63014ab4eab9e3247023ef78820ae17153d5ed
Valentin1337 <valentin.heim.pc@gmail.com>
2022-03-16 08:41:43 +0100

    dbconn war fehlerhaft

--- models/appointments.php
+++ models/appointments.php
@@ -2,7 +2,7 @@
 require_once __DIR__.'/db_connection.php';
 
 function get_appointments() {
-    $sql = "Select * from appointments";
+    $sql = "Select `date`,`begin_time`,`end_time`,`description` from appointments";
     $stmt = get_db()->query($sql);
     $res = $stmt->fetchAll();
     
--- models/db_connection.php
+++ models/db_connection.php
@@ -8,8 +8,8 @@ function get_db()
   }
 
   require_once __DIR__ . '/../config/config.php';
-  $dsn = sprintf('mysql:host=%s;dbname=%s;', 'db', 'drive2future');
-  $db = new PDO($dsn, 'root', 'root');
+  $dsn = sprintf('mysql:host=%s;dbname=%s;', DB_HOST, DB_NAME);
+  $db = new PDO($dsn, DB_USER, DB_PASS);
 
   return $db;
 }
