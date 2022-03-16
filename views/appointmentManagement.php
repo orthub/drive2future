5b0465a0c8fd8e1dbcb9e04746f7fbd48201e25c
programMiky <Michaela.Groetzl@gmx.at>
2022-03-16 10:41:00 +0100

    edit require

--- views/appointmentManagement.php
+++ views/appointmentManagement.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../lib/sessionHelper.php';
-require_once __DIR__ . '../controllers/appointments.php';
+require_once __DIR__ . '/../controllers/appointments.php';
 ?>
 
 <!DOCTYPE html>
