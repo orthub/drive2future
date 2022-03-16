9a4432e78f405ac2cde236a28f13d7b3f8c80357
Valentin1337 <valentin.heim.pc@gmail.com>
2022-03-16 07:59:15 +0100

    Appointments

--- views/appointments.php
+++ views/appointments.php
@@ -1,7 +1,7 @@
 <?php
     require_once __DIR__ . '/../lib/sessionHelper.php';
-    require_once __DIR__ . '/../controllers/appointments.php';
-    $appointments = get_appointments();
+    require_once __DIR__ .'/../controllers/appointments.php';
+    
 ?>
 
 <!DOCTYPE html>
