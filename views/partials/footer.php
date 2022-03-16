c638f2f295745c3e34a01598b5a842c4c0a5f4e4
lisas98 <stoeger_lisa@gmx.at>
2022-03-16 15:10:36 +0100

    header, footer

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,3 +1,26 @@
-<div class="footer">
-  <a href="">made by SOFTEC</a>
-</div>
+<footer>
+  <div class="footer">
+    <div class="company-info">
+      <div class="company">
+        <p>drive2future GmbH</p>
+        <p>Autostraße 1</p>
+        <p>Krems 3500</p>
+      </div>
+      <div class="contact">
+        <p><a href="mailto:info@drivetofuture.at" title="E-Mail an info@drivetofuture.at">info@drivetofuture.at</a></p>
+        <p>Tel.: 0660/12345678</p>
+        <p>Fax.: 01234/5678979</p>
+      </div>
+      <div class="links">
+        <p><a href="todo" title="Datenschutz">Datenschutz</a></p>
+        <p><a href="todo" title="Impressum">Impressum</a></p>
+        <p><a href="https://www.wko.at/" title="Zur Seite der WKO">Zur Seite der WKO</a></p>
+      </div>
+    </div>
+
+    <div class="copy">&copy; Copyright SOFTECH <script>
+        document.write(new Date().getUTCFullYear());
+      </script>
+    </div>
+  </div>
+</footer>
--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,3 +1,18 @@
-<div class="navbar">
-
-</div>
+<header>
+    <div class="navbar">
+        <div class="logo">
+            <a href="todo" title="Zur Startseite">
+                <img src="todo" width="280" />
+            </a>
+        </div>
+        <a class="nav-link" href="todo" title="Zu unserer Philosophie">Unsere Philosophie</a>
+        <a class="nav-link" href="todo" title="Zu unserem Team">Wer sind wir?</a>
+        <a class="nav-link" href="todo" title="Zu unserem Shop">Zum Shop</a>
+        <?php
+        /*if (isset($_SESSION['isStaff']) or isset($_SESSION['customerRoles'])) { ?>
+            <a class="nav-link" href="logout" title="Logout">Logout</a>
+        <?php }*/
+        ?>
+    </div>
+    <img class="header-img" src="public/images/BannerSeife.jpg" width="1920" />
+</header>
