55d835987f507d2a9b9744ca177c6bba848fdaf6
lisas98 <stoeger_lisa@gmx.at>
2022-03-17 10:57:03 +0100

    change header

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -5,14 +5,13 @@
                 <img src="todo" width="280" />
             </a>
         </div>
-        <a class="nav-link" href="todo" title="Zu unserer Philosophie">Unsere Philosophie</a>
-        <a class="nav-link" href="todo" title="Zu unserem Team">Wer sind wir?</a>
-        <a class="nav-link" href="todo" title="Zu unserem Shop">Zum Shop</a>
+        <a class="nav-link" href="todo" title="Zur Terminübersicht">Terminübersicht</a>
+        <a class="nav-link" href="todo" title="Zu den Unterlagen">Unterlagen</a>
         <?php
         /*if (isset($_SESSION['isStaff']) or isset($_SESSION['customerRoles'])) { ?>
             <a class="nav-link" href="logout" title="Logout">Logout</a>
         <?php }*/
         ?>
     </div>
-    <img class="header-img" src="public/images/BannerSeife.jpg" width="1920" />
+    <!--<img class="header-img" src="public/images/BannerSeife.jpg" width="1920" />-->
 </header>
