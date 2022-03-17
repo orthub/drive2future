112acf603808596d9a3e48140e413b9ee1887bfe
Valentin1337 <valentin.heim.pc@gmail.com>
2022-03-17 15:23:40 +0100

    Dokument verwaltung Funktionsfähig

--- controllers/files.php
+++ controllers/files.php
@@ -2,25 +2,80 @@
 require_once __DIR__ .'/../models/db_connection.php';
 require_once __DIR__ .'/../models/files.php';
 
+
+//$userrole = check_user_role()
+
 $files = get_files();
 
-if(isset($_FILES['userfile']['name']) && $_FILES['userfile']['tmp_name'] != null){
+if(!empty($_FILES['userfile']['name']) && $_FILES['userfile']['tmp_name'] != null){
     $filename = basename($_FILES['userfile']['name']);
     $uploadfile = __DIR__ .'/../Documents/' . $filename;
-    
-    $today = date("Y-m-d");
 
-    if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-        $res = add_New_File($filename,$today);
-
-        add_New_File($filename,$today);
-        //überprüfung ob funktioniert hat fehlt
+    if(file_exists($uploadfile)){
+        $_SESSION['errors']['File'] = 'File ist schon vorhanden';
+        header('location: /drive2future/views/manageDocs.php');
+    }
+    else{
+        if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+            if(add_New_File($filename)){
+                header('location: /drive2future/views/manageDocs.php');
+            }
+        }
+    }
+}
+if(isset($_POST['delete'])){
+    $filename = get_filename_to_id($_POST['delete']);
+    $filepath = __DIR__.'\\..\\Documents\\'.$filename;
+    if(file_exists($filepath)){
+        if(!unlink($filepath)){
+            $_SESSION['errors']['File'] = 'Fehler beim löschen des Files';
+            header('location: /drive2future/views/manageDocs.php');
+        }
+    }
+    if(delete_document_entry($_POST['delete'])){
+        header('location: /drive2future/views/manageDocs.php');
+    }
+    else{
+        $_SESSION['errors']['File'] = 'Fehler beim löschen des DB Eintrags';
         header('location: /drive2future/views/manageDocs.php');
     }
 }
+if(isset($_POST['download'])){
+    $file_to_download = '..\\Documents\\'.$_POST['download'];
 
-else if(isset($_POST['delete'])){
-    delete_file($_POST['delete']);
-    //überprüfung ob funktioniert hat fehlt
-    header('location: /drive2future/views/manageDocs.php');
+    if (!file_exists($file_to_download)) {
+        $_SESSION['errors']['File'] = 'File Existiert nicht';
+        header('location: /drive2future/views/manageDocs.php');
+        exit();
+    }
+
+    if (!is_file($file_to_download)) {
+        $_SESSION['errors']['File'] = 'File ist nicht gültig';
+        header('location: /drive2future/views/manageDocs.php');
+        exit();
+    }
+
+    $client_file = $_POST['download'];
+
+    $download_rate = 200; // 200Kb/s
+
+    $f = null;
+
+    header('Cache-control: private');
+	header('Content-Type: application/octet-stream');
+	header('Content-Length: ' . filesize($file_to_download));
+	header('Content-Disposition: filename=' . $_POST['download']);
+
+    flush();
+
+    $f = fopen($file_to_download, 'r');
+
+    while (!feof($f)) {
+        echo fread($f, round($download_rate * 1024));
+        flush();
+    }
+}
+if (empty($_FILES['userfile']['name']) && $_SERVER['REQUEST_METHOD'] === 'POST' && !isset($_POST['download']) && !isset($_POST['delete'])) {
+    $_SESSION['errors']['File'] = 'Keine Datei ausgewählt';
+    header('Location: ' . '/drive2future/views/manageDocs.php');
 }
--- models/files.php
+++ models/files.php
@@ -2,27 +2,37 @@
 require_once __DIR__. '/../lib/sessionHelper.php';
 require_once __DIR__.'/db_connection.php';
 
-function add_New_File($path,$date)
+function add_New_File($path)
 {
-    $sql = "INSERT INTO `drive2future`.`documents` (`path`, `date`) 
-            VALUES (:Path, :date)";
+    $sql = "INSERT INTO `drive2future`.`documents` (`path`) 
+            VALUES (:Path)";
     $stmt = get_db()->prepare($sql);
-    $stmt->execute([':Path' => $path,':date' => $date]);
+    $res = $stmt->execute([':Path' => $path]);
 
-    return $stmt;
+    return $res;
 }
 
 function get_files(){
-    $sql = "select `id_documents`,`path`, `date` from drive2future.documents;";
+    $sql = "select `id_documents`,`path`, `date` from drive2future.documents order by `date` desc;";
     $stmt = get_db()->query($sql);
     $res = $stmt->fetchAll();
     
     return $res;
 }
+function get_filename_to_id($id){
+    $sql = "select `path` from drive2future.documents where id_documents = :id;";
+    $stmt = get_db()->prepare($sql);
+    $stmt->execute([':id' => $id]);
+    $res = $stmt->fetchColumn();
+    
+    return $res;
+}
 
-function delete_file($id)
+function delete_document_entry($id)
 {
     $sql = "delete from drive2future.documents where id_documents = :id;";
     $stmt = get_db()->prepare($sql);
-    $stmt->execute([':id' => $id]);
+    $res = $stmt->execute([':id' => $id]);
+
+    return $res;
 }
