ac5f67c7b4ee48602b64e32b48e58bcf03683beb
Valentin1337 <valentin.heim.pc@gmail.com>
2022-03-22 10:06:14 +0100

    Navbar Terminverwaltung hinzugefügt

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -7,8 +7,16 @@
     </div>
     <a class="nav-link" href="/drive2future/views/appointments.php" title="Zur Terminübersicht">Terminübersicht</a>
     <a class="nav-link" href="/drive2future/views/manageDocs.php" title="Zu den Unterlagen">Unterlagen</a>
-    <a class="nav-link" href="/drive2future/views/login.php">Login</a>
+
+    <?php if(isset($_SESSION['user_session'])) :?>
+    <?php if( (isset($user_employee) || isset($user_admin)) && ( $user_employee || $user_admin)) :?>
+    <a class="nav-link" href="/drive2future/views/appointmentManagement.php">Terminverwaltung</a>
+    <?php endif ?>
     <a class="nav-link" href="/drive2future/views/logout.php">Logout</a>
+    <?php else : ?>
+      <a class="nav-link" href="/drive2future/views/login.php">Login</a>
+    <?php endif ?>
+    
     <?php
         /*if (isset($_SESSION['isStaff']) or isset($_SESSION['customerRoles'])) { ?>
     <a class="nav-link" href="logout" title="Logout">Logout</a>
