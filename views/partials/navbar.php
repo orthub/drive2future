e0b430cf6115c8ec377b4258d3d0bdac0ce355d7
Valentin1337 <valentin.heim.pc@gmail.com>
2022-03-23 08:14:10 +0100

    bei der Navbarwird nurmehr angezeigt was man sehen darf

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -10,19 +10,32 @@
       <input type="checkbox" id="checkbox_toggle" />
       <label for="checkbox_toggle" class="hamburger"><img class="" src="/drive2future/assets/img/Hamburger_icon.png" width="40" /></label>
       <div class="menu">
-        <li><a class="nav-link" href="/drive2future/views/appointments.php" title="Zur Terminübersicht">Terminübersicht</a></li>
-        <li><a class="nav-link" href="/drive2future/views/manageDocs.php" title="Zu den Unterlagen">Unterlagen</a></li>
-
+        
+        
 
         <?php if (isset($_SESSION['user_session'])) : ?>
-          <?php if ((isset($user_employee) || isset($user_admin)) && ($user_employee || $user_admin)) : ?>
+
+          <?php require_once __DIR__ . '/../../lib/user_role.php';
+            if ($user_employee || $user_admin) : ?>
+
             <li><a class="nav-link" href="/drive2future/views/appointmentManagement.php">Terminverwaltung</a></li>
+
           <?php endif ?>
+
+          <li><a class="nav-link" href="/drive2future/views/appointments.php" title="Zur Terminübersicht">Terminübersicht</a></li>
+
+          <li><a class="nav-link" href="/drive2future/views/manageDocs.php" title="Zu den Unterlagen">Unterlagen</a></li>
+
           <li><a class="nav-link" href="/drive2future/views/logout.php">Logout</a></li>
+
         <?php else : ?>
+
           <li><a class="nav-link" href="/drive2future/views/login.php">Login</a></li>
+
         <?php endif ?>
+
       </div>
+      
     </ul>
 
   </div>
