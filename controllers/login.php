5d87e7d4c1d4c16e2b07bf348691df98e754b5b5
Roland.Ortner <ortner.roland@outlook.at>
2022-03-29 10:05:48 +0200

    login should work now

--- controllers/login.php
+++ controllers/login.php
@@ -1,72 +1,183 @@
 <?php
 require_once __DIR__ . '/../lib/sessionHelper.php';
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-  header('Location: ' . '/views/login.php');
-  exit();
-}
 
 if (isset($_SESSION['errors'])) {
   unset($_SESSION['errors']);
 }
 
-$valideEmail = false;
-$validePassword = false;
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+  header('Location: ' . '/views/login.php');
+  exit();
+}
+
+$emailExist = false;
+$matchPasswd = false;
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  
-require_once __DIR__ . '/../models/login.php';
+  $loginEmail = htmlspecialchars($_POST['login-mail']);
+  $filteredEmail = filter_var($loginEmail, FILTER_VALIDATE_EMAIL);
+  $loginPasswd = trim($_POST['login-passwd']);
 
-$email = htmlspecialchars($_POST["login-mail"]);
-$check_email = filter_var($email, FILTER_VALIDATE_EMAIL);
+  if (!isset($filteredEmail) || empty($loginEmail)) {
+    $_SESSION['errors']['login-mail'] = 'Bitte Email eingeben';
+    header('Location: ' . '/drive2future/views/login.php');
+    exit();
+  }
 
-$password = htmlspecialchars($_POST['login-passwd']);
+  if (!isset($loginPasswd) || empty($loginPasswd)) {
+    $_SESSION['errors']['login-passwd'] = 'Bitte Passwort eingeben';
+    header('Location: ' . '/drive2future/views/login.php');
+    exit();
+  }
 
+  require_once __DIR__ . '/../models/login.php';
 
-if (empty($email)) {
-  $_SESSION['errors']['email'] = 'Email erforderlich';
-  header('Location: ' . '/drive2future/views/login.php');
-}
+  $emailExist = search_mail($filteredEmail, $loginPasswd);
 
-if ($check_email === false) {
-  $_SESSION['errors']['email'] = 'Bitte eine Valide Email-Adresse eingeben';
-  header('Location: ' . '/drive2future/views/login.php');
-}
+  if (!$emailExist) {
+    $_SESSION['errors']['login-fail'] = 'Email oder Passwort stimmen nicht';
+    header('Location: ' . '/drive2future/views/login.php');
+    exit();
+  }
 
-if (!empty($password)) {
-  $validePassword = true;
-}
+  if ($emailExist) {
+    $matchPasswd = match_mail_password($filteredEmail, $loginPasswd);
 
-if (empty($password)) {
-  $_SESSION['errors']['password'] = 'Passwort erforderlich';
-  $validePassword = false;
-}
-
-$check_mail_exist = search_mail($email);
-if ($check_mail_exist) {
-  $valideEmail = true;
-}
-
-if ($valideEmail === true && $validePassword === true) {
-  $match_mail_passwd = match_mail_password($email);
-  
-  
-  if ($match_mail_passwd === false) {
-      $_SESSION['errors']['login-fail'] = 'Benutzer oder Passwort stimmen nicht.';
+    if (!$matchPasswd) {
+      $_SESSION['errors']['login-fail'] = 'Email oder Passwort stimmen nicht';
       header('Location: ' . '/drive2future/views/login.php');
+      exit();
     }
 
-    if ($match_mail_passwd) {
-      $user_id = get_user_id($email);
-      
+    if ($matchPasswd) {
       // bitte drinnen lassen sonst geht der code von anderen nicht mehr
       $_SESSION['user_id'] = $user_id;
-      
+  
       $_SESSION['user_session'] = $user_id . '_loggedIn';
       header('Location: ' . '/drive2future/views/appointments.php');
+      exit();
     }
-  }
-  if (!empty($_SESSION['errors'])) {
-    header('Location: ' . '/drive2future/views/login.php');
+  header('Location: ' . '/drive2future/views/login.php');
+  exit();
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  
+//   require_once __DIR__ . '/../models/login.php';
+
+
+// $email = htmlspecialchars($_POST["login-mail"]);
+// $check_email = filter_var($email, FILTER_VALIDATE_EMAIL);
+
+// $password = htmlspecialchars($_POST['login-passwd']);
+
+
+//   if (isset($email)) {
+//     $_SESSION['errors']['email'] = 'Email erforderlich';
+//     header('Location: ' . '/drive2future/views/login.php');
+// }
+
+//   if ($check_email === false) {
+//     $_SESSION['errors']['email'] = 'Bitte eine Valide Email-Adresse eingeben';
+//     header('Location: ' . '/drive2future/views/login.php');
+//   }
+
+//   if (!isset($password)) {
+//     $validePassword = true;
+//   }
+
+//   if (isset($password)) {
+//     $_SESSION['errors']['password'] = 'Passwort erforderlich';
+//     $validePassword = false;
+//   }
+
+// $check_mail_exist = search_mail($email);
+
+//   if ($check_mail_exist) {
+//     $valideEmail = true;
+//   }
+
+//   if ($valideEmail === true && $validePassword === true) {
+//     $match_mail_passwd = match_mail_password($email, $password);
+
+  
+  
+//     if ($match_mail_passwd === false) {
+//         $_SESSION['errors']['login-fail'] = 'Benutzer oder Passwort stimmen nicht.';
+//         header('Location: ' . '/drive2future/views/login.php');
+//       }
+
+//     if ($match_mail_passwd) {
+//       $user_id = get_user_id($email);
+      
+//       // bitte drinnen lassen sonst geht der code von anderen nicht mehr
+//       $_SESSION['user_id'] = $user_id;
+      
+//       $_SESSION['user_session'] = $user_id . '_loggedIn';
+//       header('Location: ' . '/drive2future/views/appointments.php');
+//     }
+//   }
+  
+//   if (!empty($_SESSION['errors'])) {
+//     header('Location: ' . '/drive2future/views/login.php');
+//   }
+// }
