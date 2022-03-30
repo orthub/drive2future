ad55e860baa8746ddb632a822cdb3a2e60fdb67b
Roland.Ortner <ortner.roland@outlook.at>
2022-03-30 08:11:00 +0200

    register status

--- models/register.php
+++ models/register.php
@@ -10,44 +10,46 @@ function search_if_email_exists_already()
   return $res_search_if_existing_email;
 }
 
-function create_new_user(string $first_name, string $last_name, string $email, string $passwd, /*string $status*/)
+function create_new_user(string $first_name, string $last_name, string $email, string $passwd)
 {
+  $status = 'aktiv';
   $passwd = password_hash($passwd, PASSWORD_DEFAULT);
   $sql_create_new_user = 'INSERT INTO `users` 
                           SET `first_name` = :firstName,
                               `last_name` = :lastName,
                               `email` = :email,
                               `password` = :passwd,
-                              `status` = " ",
+                              `status` = :userStatus,
                               `roles_id_role` = 2';
   $stmt_create_new_user = get_db()->prepare($sql_create_new_user);
   $stmt_create_new_user->execute([
     ':firstName' => $first_name,
     ':lastName' => $last_name,
     ':email' => $email,
-    ':passwd' => $passwd
-    // ':userStatus' => $status
+    ':passwd' => $passwd,
+    ':userStatus' => $status
   ]);
   return $stmt_create_new_user;
 }
 
-function create_new_employee(string $first_name, string $last_name, string $email, string $passwd, /*string $status*/)
+function create_new_employee(string $first_name, string $last_name, string $email, string $passwd)
 {
+  $status = 'aktiv';
   $passwd = password_hash($passwd, PASSWORD_DEFAULT);
   $sql_create_new_employee = 'INSERT INTO `users` 
                           SET `first_name` = :firstName,
                               `last_name` = :lastName,
                               `email` = :email,
                               `password` = :passwd,
-                              `status` = " ",
+                              `status` = :userStatus,
                               `roles_id_role` = 3';
   $stmt_create_new_employee = get_db()->prepare($sql_create_new_employee);
   $stmt_create_new_employee->execute([
     ':firstName' => $first_name,
     ':lastName' => $last_name,
     ':email' => $email,
-    ':passwd' => $passwd
-    // ':userStatus' => $status
+    ':passwd' => $passwd,
+    ':userStatus' => $status
   ]);
   return $stmt_create_new_employee;
 }
